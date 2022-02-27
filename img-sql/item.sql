39ea63dcd1a314b54ae364c164b450c23042aa31
nezch <94806250+nezch@users.noreply.github.com>
2022-02-27 19:16:10 +0300

    Create item.sql

---
+++ img-sql/item.sql
@@ -0,0 +1,7 @@
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+	('sausage', 'Sosis', 1, 0, 1),
+	('paketsosisli', 'Paketlenmiş Sosisli', 1, 0, 1),
+	('hotdog', 'Sosisli', 1, 0, 1),
+	('hotdogbread', 'Sosisli Ekmeği', 1, 0, 1),
+	('hotdogmalzeme', 'Sosisli Malzemesi', 1, 0, 1)
+;
