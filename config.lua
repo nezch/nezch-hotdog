84ea29ee036d2d582aed2f4bbcdfb27d0c7bf630
nezch <94806250+nezch@users.noreply.github.com>
2022-02-27 19:15:12 +0300

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,19 @@
+Config = {}
+
+Config.MinParaMiktar = 360 -- Minimum dağıtımda verilecek para miktarı
+Config.MaxParaMiktar = 795 -- Maximum dağıtımda verilecek para miktarı
+
+Config.SosisPisirme = {x = 1592.689, y = 6455.921, z = 26.013} -- ET Pişirme kordinat
+Config.SosisliHazirla = {x = 1594.827, y = 6454.807, z = 26.013} -- Sosisli Hazırlama kordinat
+Config.SosisliPaketle = {x = 1588.297, y = 6457.936, z = 26.013} -- Taco Paketleme kordinat
+Config.SosisliDagitim = {x = 1590.454, y = 6455.998, z = 26.013} -- Taco Dağıtım kordinat
+Config.SosisliEkmek = {x = 1586.087, y = 6458.910, z = 26.013} -- Taco Ekmek Isıtma kordinat
+Config.SosisliMalzeme = {x = 1597.663, y = 6453.002, z = 25.317} -- Sosisli Malzeme kordinat
+
+Config.Blip = {
+    coords = vector3(1590.209, 6453.979, 26.013), -- Blip kordinat
+    sekil = 80,
+    boyut = 0.6,
+    renk = 33,
+    yazi = 'Hotdogcu Javes'
+}
